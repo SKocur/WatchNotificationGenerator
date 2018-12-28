7aa79833c4e73c7248cf0ceb5e32db195d6f6b72
skocur <skocur10@gmail.com>
2018-12-28 16:22:08 +0100

    Add button to list item which is responsible for displaying Notification

--- app/src/main/java/com/skocur/watchnotificationgenerator/HomeActivity.java
+++ app/src/main/java/com/skocur/watchnotificationgenerator/HomeActivity.java
@@ -1,8 +1,5 @@
 package com.skocur.watchnotificationgenerator;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,35 +8,26 @@ import android.widget.ListView;
 
 import com.skocur.watchnotificationgenerator.adapters.CategoriesAdapter;
 import com.skocur.watchnotificationgenerator.models.Category;
-import com.skocur.watchnotificationgenerator.models.Notification;
 import com.skocur.watchnotificationgenerator.sqlutils.DatabaseService;
 import com.skocur.watchnotificationgenerator.utils.CustomAddAlertDialog;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private static final String NOTIFICATIONS_CHANNEL = "s0x";
-    private static int NOTIFICATIONS_COUNTER = 0;
     public static DatabaseService databaseService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        createNotificationChannel();
         setContentView(R.layout.activity_home);
 
         databaseService = new DatabaseService(getApplicationContext());
 
         initiateViews();
-
-        generateNotificationsFromEverything();
     }
 
     private void initiateViews() {
@@ -72,56 +60,5 @@ public class HomeActivity extends AppCompatActivity {
                 }).build();
             }
         });
-    }
-
-    private void generateNotificationsFromEverything() {
-        try {
-            List<Notification> notifications = databaseService.getAllNotifications();
-            for (Notification notification : notifications) {
-                //Thread.sleep(1000);
-                displayNotification(notification);
-                Log.e("notification", notification.toString());
-            }
-        } catch (InterruptedException e) {
-            Log.e("!", e.toString());
-        } catch (ExecutionException e) {
-            Log.e("!", e.toString());
-        } catch (NullPointerException e) {
-            Log.e("!", e.toString());
-        }
-    }
-
-    /**
-     * IMPORTANT NOTE
-     * User has to add this app to "allowed applications' on watch settings.
-     *
-     * @param notification
-     */
-    private void displayNotification(Notification notification) {
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, NOTIFICATIONS_CHANNEL)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle(notification.getNotificationTitle())
-                .setContentText(notification.notificationContent)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .extend(new NotificationCompat.WearableExtender().setHintShowBackgroundOnly(true));
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        notificationManager.notify(NOTIFICATIONS_COUNTER++, mBuilder.build());
-    }
-
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = NOTIFICATIONS_CHANNEL;
-            String description = "Notifications";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(NOTIFICATIONS_CHANNEL, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
     }
 }
--- app/src/main/java/com/skocur/watchnotificationgenerator/adapters/NotificationsAdapter.java
+++ app/src/main/java/com/skocur/watchnotificationgenerator/adapters/NotificationsAdapter.java
@@ -1,11 +1,15 @@
 package com.skocur.watchnotificationgenerator.adapters;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.skocur.watchnotificationgenerator.R;
@@ -15,11 +19,18 @@ import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class NotificationsAdapter extends ArrayAdapter<Notification> {
 
+    private static final String NOTIFICATIONS_CHANNEL = "s0x";
+    private static int NOTIFICATIONS_COUNTER = 0;
+
     public NotificationsAdapter(Context context, ArrayList<Notification> notifications) {
         super(context, 0, notifications);
+
+        createNotificationChannel();
     }
 
     @NonNull
@@ -46,6 +57,49 @@ public class NotificationsAdapter extends ArrayAdapter<Notification> {
             }
         });
 
+        ImageView ivCreateNotification = convertView.findViewById(R.id.item_general_create_notification);
+        ivCreateNotification.setVisibility(View.VISIBLE);
+        ivCreateNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayNotification(notification);
+            }
+        });
+
         return convertView;
+    }
+
+    /**
+     * IMPORTANT NOTE
+     * User has to add this app to "allowed applications' on watch settings.
+     *
+     * @param notification
+     */
+    private void displayNotification(Notification notification) {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getContext(), NOTIFICATIONS_CHANNEL)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(notification.getNotificationTitle())
+                .setContentText(notification.notificationContent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .extend(new NotificationCompat.WearableExtender().setHintShowBackgroundOnly(true));
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getContext());
+        notificationManager.notify(NOTIFICATIONS_COUNTER++, mBuilder.build());
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = NOTIFICATIONS_CHANNEL;
+            String description = "Notifications";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(NOTIFICATIONS_CHANNEL, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getContext().getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
